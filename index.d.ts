c54366fcb9d412ed3752f7b7a4e15fe0fdb4e005
Jaco Labuschagne <zodimo@gmail.com>
2022-12-20 20:19:22 +0200

    remove CardanocliJs namespace

--- index.d.ts
+++ index.d.ts
@@ -1,56 +1,64 @@
 // Typescript type definitions for cardanocli-js
 
 export default class CardanocliJs {
-  constructor(options: CardanocliJs.ConstructorOptions);
+  constructor(options: ConstructorOptions);
 
   queryProtocolParameters(): any;
-  queryTip(): CardanocliJs.QueryTip;
-  queryStakeAddressInfo(address: string): CardanocliJs.StakeAddressInfo[];
-  queryUtxo(address: string): CardanocliJs.Utxo[];
+  queryTip(): QueryTip;
+  queryStakeAddressInfo(address: string): StakeAddressInfo[];
+  queryUtxo(address: string): Utxo[];
 
-  addressKeyGen(account: string): CardanocliJs.Account;
+  addressKeyGen(account: string): Account;
 
-  stakeAddressKeyGen(account: string): CardanocliJs.Account;
+  stakeAddressKeyGen(account: string): Account;
   stakeAddressBuild(account: string): string;
 
-  addressBuild(account: string, options: CardanocliJs.AddressBuildOptions): string;
+  addressBuild(
+    account: string,
+    options: AddressBuildOptions
+  ): string;
   addressKeyHash(account: string): string;
-  addressInfo(address: string): CardanocliJs.AddressInfo;
+  addressInfo(address: string): AddressInfo;
   addressBuildScript(script: any): string;
 
-  wallet(account: string): CardanocliJs.Wallet;
+  wallet(account: string): Wallet;
 
-  pool(poolName: string): CardanocliJs.Pool;
+  pool(poolName: string): Pool;
 
   stakeAddressRegistrationCertificate(account: string): string;
   stakeAddressDeregistrationCertificate(accout: string): string;
   stakeAddressDelegationCertificate(account: string, poolId: string): string;
   stakeAddressKeyHash(account: string): string;
 
-  nodeKeyGenKES(poolName: string): CardanocliJs.Account;
-  nodeKeyGen(poolName: string): CardanocliJs.Account;
+  nodeKeyGenKES(poolName: string): Account;
+  nodeKeyGen(poolName: string): Account;
   nodeIssueOpCert(poolName: string, kesPeriod: number): string;
-  nodeKeyGenVRF(poolName: string): CardanocliJs.Account;
+  nodeKeyGenVRF(poolName: string): Account;
   nodeNewCounter(poolName: string, counter: string): string;
 
   stakePoolId(poolName: string): string;
   stakePoolMetadataHash(metadata: string): string;
-  stakePoolRegistrationCertificate(poolName: string, options: CardanocliJs.StakePoolRegistrationOptions): string;
+  stakePoolRegistrationCertificate(
+    poolName: string,
+    options: StakePoolRegistrationOptions
+  ): string;
   stakePoolDeregistrationCertificate(poolName: string, epoch: number): string;
 
-  transactionBuildRaw(options: CardanocliJs.Transaction): string;
-  transactionBuild(options: CardanocliJs.Transaction): string;
-  transactionCalculateMinFee(options: CardanocliJs.CalculateMinFeeOptions): number;
+  transactionBuildRaw(options: Transaction): string;
+  transactionBuild(options: Transaction): string;
+  transactionCalculateMinFee(
+    options: CalculateMinFeeOptions
+  ): number;
   transactionPolicyid(script: any): string;
   transactionHashScriptData(script: any): string;
-  transactionSign(options: CardanocliJs.TransationSignOptions): string;
-  transactionWitness(options:CardanocliJs.TransactionWitnessOptions): string;
-  transactionAssemble(options: CardanocliJs.TransactionAssembleOptions): string;
+  transactionSign(options: TransationSignOptions): string;
+  transactionWitness(options: TransactionWitnessOptions): string;
+  transactionAssemble(options: TransactionAssembleOptions): string;
   transactionCalculateMinValue(value: number): number;
   transactionCalculateMinRequiredUtxo(address: string, value: any): number;
   transactionSubmit(tx: string): string;
-  transactionTxid(options: CardanocliJs.TransactionViewOptions): string;
-  transactionView(options: CardanocliJs.TransactionViewOptions): string;
+  transactionTxid(options: TransactionViewOptions): string;
+  transactionView(options: TransactionViewOptions): string;
 
   KESPeriod(): number;
   getDownloadUrl(filePath: string): string;
@@ -59,173 +67,170 @@ export default class CardanocliJs {
   toAda(lovelace: number): number;
 }
 
-declare namespace CardanocliJs {
+export interface ConstructorOptions {
+  shelleyGenesisPath: string;
+  socketPath?: string;
+  cliPath?: string;
+  dir?: string;
+  era?: string;
+  network?: string;
+  httpProvider?: string;
+}
 
-  export interface ConstructorOptions {
-    shelleyGenesisPath: string,
-    socketPath?: string,
-    cliPath?: string,
-    dir?: string,
-    era?: string,
-    network?: string,
-    httpProvider?: string,
-  }
+export interface AddressBuildOptions {
+  paymentVkey?: string;
+  stakeVkey?: string;
+  paymentScript?: string;
+  stakeScript?: string;
+}
 
-  export interface AddressBuildOptions {
-    paymentVkey?: string,
-    stakeVkey?: string,
-    paymentScript?: string,
-    stakeScript?: string,
-  }
+export interface StakePoolRegistrationOptions {
+  pledge: number;
+  margin: number;
+  cost: number;
+  url: string;
+  metaHash: string;
+  rewardAccount: string;
+  owners: string[];
+  relays: any[];
+}
 
-  export interface StakePoolRegistrationOptions {
-    pledge: number,
-    margin: number,
-    cost: number,
-    url: string,
-    metaHash: string,
-    rewardAccount: string,
-    owners: string[],
-    relays: any[]
-  }
+export interface CalculateMinFeeOptions {
+  txBody: string;
+  txIn: TxIn[];
+  txOut: TxOut[];
+  witnessCount: number;
+}
 
-  export interface CalculateMinFeeOptions {
-    txBody: string,
-    txIn: TxIn[],
-    txOut: TxOut[],
-    witnessCount: number,
-  }
+export interface TransationSignOptions {
+  signingKeys: string[];
+  txBody: string;
+}
 
-  export interface TransationSignOptions {
-    signingKeys: string[],
-    txBody: string,
-  }
+export interface TransactionWitnessOptions {
+  txBody: string;
+  signingKey: string;
+}
 
-  export interface TransactionWitnessOptions {
-    txBody: string,
-    signingKey: string,
-  }
+export interface TransactionAssembleOptions {
+  txBody: string;
+  witnessFiles: string[];
+}
 
-  export interface TransactionAssembleOptions {
-    txBody: string,
-    witnessFiles: string[],
-  }
+export interface TransactionViewOptions {
+  txBody?: string;
+  txFile?: string;
+}
 
-  export interface TransactionViewOptions {
-    txBody?: string,
-    txFile?: string,
-  }
+export interface Transaction {
+  txIn: TxIn[];
+  txOut: TxOut[];
+  txInCollateral?: TxInCollateral[];
+  withdrawals?: Withdrawal[];
+  certs?: Certificate[];
+  fee?: number;
+  mint?: Mint[];
+  auxScript?: any;
+  metadata?: any;
+  invalidBefore?: number;
+  invalidAfter?: number;
+  scriptInvalid?: boolean;
+}
 
-  export interface Transaction {
-    txIn: TxIn[],
-    txOut: TxOut[],
-    txInCollateral?: TxInCollateral[],
-    withdrawals?: Withdrawal[],
-    certs?: Certificate[],
-    fee?: number,
-    mint?: Mint[],
-    auxScript?: any,
-    metadata?: any,
-    invalidBefore?: number,
-    invalidAfter?: number,
-    scriptInvalid?: boolean,
-  }
+export interface QueryTip {
+  epoch: number;
+  hash: string;
+  slot: number;
+  block: number;
+  era: string;
+  syncProgress: string;
+}
 
-  export interface QueryTip {
-    epoch: number,
-    hash: string,
-    slot: number,
-    block: number,
-    era: string,
-    syncProgress: string,
-  }
+export interface Wallet {
+  name: string;
+  paymentAddr: string;
+  stakingAddr: string;
+  balance: () => {
+    utxo: Utxo[];
+    value: any;
+  };
+  reward: () => StakeAddressInfo | string;
+}
 
-  export interface Wallet {
-    name: string,
-    paymentAddr: string,
-    stakingAddr: string,
-    balance: () => {
-      utxo: Utxo[],
-      value: any
-    },
-    reward: () => StakeAddressInfo | string,
-  }
+export interface Pool {
+  name: string;
+  id: string;
+}
 
-  export interface Pool {
-    name: string,
-    id: string,
-  }
+export interface StakeAddressInfo {
+  address: string;
+  rewardAccountBalance: number;
+  delegation: string;
+}
 
-  export interface StakeAddressInfo {
-    address: string,
-    rewardAccountBalance: number,
-    delegation: string,
-  }
+export interface AddressInfo {
+  address: string;
+  era: string;
+  encoding: string;
+  type: string;
+  base16: string;
+}
 
-  export interface AddressInfo {
-    address: string,
-    era: string,
-    encoding: string,
-    type: string,
-    base16: string,
-  }
+export interface TxIn {
+  txHash: string;
+  txId: string;
+  script?: any;
+  datum?: any;
+  redeemer?: any;
+  executionUnits: any;
+}
 
-  export interface TxIn {
-    txHash: string,
-    txId: string,
-    script?: any,
-    datum?: any,
-    redeemer?: any,
-    executionUnits: any,
-  }
+export interface TxOut {
+  address: string;
+  value: any;
+  datumHash: string;
+}
 
-  export interface TxOut {
-    address: string,
-    value: any,
-    datumHash: string,
-  }
+export interface TxInCollateral {
+  txHash: string;
+  txId: string;
+}
 
-  export interface TxInCollateral {
-    txHash: string,
-    txId: string,
-  }
+export interface Withdrawal {
+  stakingAddress: string;
+  reward: number;
+  script: any;
+  datum: any;
+  redeemer: any;
+  executionUnits: any;
+}
 
-  export interface Withdrawal {
-    stakingAddress: string,
-    reward: number,
-    script: any,
-    datum: any,
-    redeemer: any,
-    executionUnits: any,
-  }
+export interface Certificate {
+  cert: string;
+  script: any;
+  datum: any;
+  redeemer: any;
+  executionUnits: any;
+}
 
-  export interface Certificate {
-    cert: string,
-    script: any,
-    datum: any,
-    redeemer: any,
-    executionUnits: any,
-  }
+export interface Mint {
+  action: string;
+  quantity: string;
+  asset: string;
+  script: any;
+  datum: any;
+  redeemer: any;
+  executionUnits: any;
+}
 
-  export interface Mint {
-    action: string,
-    quantity: string,
-    asset: string,
-    script: any,
-    datum: any,
-    redeemer: any,
-    executionUnits: any,
-  }
+export interface Utxo {
+  txHash: string;
+  txId: number;
+  value: any;
+}
 
-  export interface Utxo {
-    txHash: string,
-    txId: number,
-    value: any
-  }
-
-  export interface Account {
-    vkey: string,
-    skey: string,
-    counter?: string,
-  }
+export interface Account {
+  vkey: string;
+  skey: string;
+  counter?: string;
 }
